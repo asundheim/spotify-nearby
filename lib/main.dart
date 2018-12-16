e7ddefccbc49af80723192156a37535c3f3dac44
Anders Sundheim <andersundheim@gmail.com>
2018-12-16 17:17:08 -0600

    edited main

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your application..
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
