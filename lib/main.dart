af2d0c149853a2ce995580db35496495d094e9af
Anders Sundheim <andersundheim@gmail.com>
2019-01-03 16:53:25 -0600

    Fixed no song playing error

--- lib/main.dart
+++ lib/main.dart
@@ -80,6 +80,7 @@ class MyAppState extends State<MyApp> {
   // Accesses theme data stored in shared preferences "darkMode"
   Future<void> _loadDarkMode() async {
     final SharedPreferences prefs = await getStorageInstance();
+    print('reloaded');
     setState(() {
       _isDark = themeService.darkThemeEnabled(prefs);
     });
